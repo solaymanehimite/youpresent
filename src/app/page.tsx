7cdc97c8eb1b53778834fa30487d28505e4994f3
Solaymane Himite <solham.1122@gmail.com>
2025-11-17 12:39:39 +0100

    fix mobile sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Form } from "@/components/Form";
 
 export default function Home() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <div className="flex min-h-screen items-center justify-center p-10 sm:px-0 bg-zinc-50 font-sans dark:bg-black">
             <Form />
         </div>
     );
