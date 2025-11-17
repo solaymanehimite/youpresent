ef5524ab7df3e20b3871b1b446b5397cb8cd1d7a
Solaymane Himite <solham.1122@gmail.com>
2025-11-17 23:01:32 +0100

    fix naming and add steps

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { FormData } from "@/lib/types"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import {
@@ -5,133 +8,43 @@ import {
     FieldDescription,
     FieldGroup,
     FieldLabel,
-    FieldLegend,
-    FieldSeparator,
     FieldSet,
     FieldTitle,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
+import { useState } from "react"
 
 export function Form() {
+    const [formData, setFormData] = useState<FormData>({
+        firstName: "",
+        lastName: "",
+        number: "",
+        dueDate: new Date(),
+
+        presentationDescription: "",
+        slides: 5,
+        isAnimated: false,
+        theme: "black"
+    });
+
+    const [currentStep, setCurrentStep] = useState<number>(0);
+
     return (
         <div className="w-full max-w-md">
             <form>
-                <FieldGroup>
+                <FieldGroup className="h-96">
                     <FieldSet>
-                        <FieldGroup>
-                            <FieldTitle className="text-2xl">Personal Information</FieldTitle>
-                            <Field>
-                                <FieldLabel htmlFor="checkout-7j9-card-name-43j">
-                                    Full Name*
-                                </FieldLabel>
-                                <Input
-                                    id="checkout-7j9-card-name-43j"
-                                    placeholder="Solaymane Himite"
-                                    required
-                                />
-                            </Field>
-                            <Field>
-                                <FieldLabel htmlFor="checkout-7j9-card-name-43j">
-                                    Whatsapp Number*
-                                </FieldLabel>
-                                <Input
-                                    id="checkout-7j9-card-name-43j"
-                                    placeholder="0612345678"
-                                    required
-                                />
-                            </Field>
-                        </FieldGroup>
-                        <FieldSeparator />
-                        <FieldGroup>
-                            <FieldTitle className="text-2xl">Customize You're Presentation</FieldTitle>
-                            <Field>
-                                <FieldLabel htmlFor="checkout-7j9-card-number-uw1">
-                                    What's you're Presenation about?*
-                                </FieldLabel>
-                                <Textarea
-                                    id="checkout-7j9-card-number-uw1"
-                                    placeholder="computer parts and their uses...."
-                                    required
-                                />
-                                <FieldDescription>
-                                    *more than 100 characters
-                                </FieldDescription>
-                            </Field>
-                            <FieldLabel htmlFor="deadline-presentation">
-                                Presentation Deadline*
-                            </FieldLabel>
-                            <div className="flex gap-4">
-                                <Field id="deadline-presentation">
-                                    <FieldLabel htmlFor="deadline-month">
-                                        Month
-                                    </FieldLabel>
-                                    <Select defaultValue="" required>
-                                        <SelectTrigger id="deadline-month">
-                                            <SelectValue placeholder="Month" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            {Array.from({ length: 12 }, (_, index) => (
-                                                <SelectItem value={`${index + 1}`} key={-index}>{index + 1}</SelectItem>
-                                            ))}
-                                        </SelectContent>
-                                    </Select>
-                                </Field>
-                                <Field>
-                                    <FieldLabel htmlFor="deadline-year">
-                                        Year
-                                    </FieldLabel>
-                                    <Select defaultValue="2025" required>
-                                        <SelectTrigger id="deadline-year">
-                                            <SelectValue placeholder="Year" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            <SelectItem value="2025">2025</SelectItem>
-                                            <SelectItem value="2026">2026</SelectItem>
-                                        </SelectContent>
-                                    </Select>
-                                </Field>
-                                <Field>
-                                    <FieldLabel htmlFor="deadline-day">
-                                        Day
-                                    </FieldLabel>
-                                    <Select defaultValue="" required>
-                                        <SelectTrigger id="deadline-day">
-                                            <SelectValue placeholder="Day" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            {Array.from({ length: 31 }, (_, index) => (
-                                                <SelectItem value={`${index + 1}`} key={index}>{index + 1}</SelectItem>
-                                            ))}
-                                        </SelectContent>
-                                    </Select>
-                                </Field>
-                            </div>
-                            <Field orientation="horizontal">
-                                <Checkbox id="enable-animations" />
-                                <FieldLabel htmlFor="enable-animations">
-                                    Animations and Transitions
-                                </FieldLabel>
-                            </Field>
-                            <Field orientation="horizontal">
-                                <Input type="number" className="w-30" defaultValue={5} />
-                                <FieldLabel>
-                                    Number of Slides
-                                </FieldLabel>
-                            </Field>
-                        </FieldGroup>
+                        {currentStep == 0 && <PersonalInfo formData={formData} setFormData={setFormData} />}
+                        {currentStep == 1 && <PresentationInfo formData={formData} setFormData={setFormData} />}
                     </FieldSet>
-                    <Field orientation="horizontal">
-                        <Button type="submit">Submit</Button>
-                        <Button variant="outline" type="button">
-                            Cancel
+                    <Field orientation="horizontal" className="mt-auto w-full flex justify-between">
+                        <Button type="button" onClick={_ => setCurrentStep(prev => prev - 1)}
+                            disabled={currentStep == 0} variant="outline">
+                            Back
+                        </Button>
+                        <Button type="button" onClick={() => setCurrentStep(Math.min(currentStep + 1, 1))}>
+                            {currentStep < 1 ? "Next" : "Submit"}
                         </Button>
                     </Field>
                 </FieldGroup>
@@ -140,3 +53,91 @@ export function Form() {
     )
 }
 
+
+function PersonalInfo({
+    formData,
+    setFormData
+}: {
+    formData: FormData
+    setFormData: any
+}) {
+    return <FieldGroup>
+        <FieldTitle className="text-2xl">Personal Information</FieldTitle>
+        <Field>
+            <div id="name-input" className="flex flex-col sm:flex gap-4">
+                <div className="flex flex-col gap-2">
+                    <FieldLabel htmlFor="first-name">
+                        First Name
+                    </FieldLabel>
+                    <Input
+                        id="first-name"
+                        placeholder="John"
+                        value={formData.firstName}
+                        onChange={(e) => setFormData({ ...formData, firstName: e.target.value, })}
+                        required
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <FieldLabel htmlFor="last-name">
+                        Last Name
+                    </FieldLabel>
+                    <Input
+                        id="last-name"
+                        placeholder="doe"
+                        value={formData.lastName}
+                        onChange={(e) => setFormData({ ...formData, lastName: e.target.value, })}
+                        required
+                    />
+                </div>
+            </div>
+        </Field>
+        <Field>
+            <FieldLabel htmlFor="whatsapp-number">
+                Whatsapp Number
+            </FieldLabel>
+            <Input
+                id="whatsapp-number"
+                placeholder="0612345678"
+                value={formData.number}
+                onChange={(e) => setFormData({ ...formData, number: e.target.value, })}
+                required
+            />
+        </Field>
+    </FieldGroup>
+}
+
+function PresentationInfo({
+    formData,
+    setFormData
+}: {
+    formData: FormData
+    setFormData: any
+}) {
+    return <FieldGroup>
+        <FieldTitle className="text-2xl">Customize You're Presentation</FieldTitle>
+        <Field>
+            <FieldLabel htmlFor="presentation-description">
+                What's you're Presenation about?*
+            </FieldLabel>
+            <Textarea
+                id="presentation-description"
+                placeholder="computer parts and their uses...."
+            />
+            <FieldDescription>
+                *more than 100 characters
+            </FieldDescription>
+        </Field>
+        <Field orientation="horizontal">
+            <Checkbox id="enable-animations" />
+            <FieldLabel htmlFor="enable-animations">
+                Animations and Transitions
+            </FieldLabel>
+        </Field>
+        <Field orientation="horizontal">
+            <Input type="number" className="w-30" defaultValue={5} />
+            <FieldLabel>
+                Number of Slides
+            </FieldLabel>
+        </Field>
+    </FieldGroup>
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export interface FormData {
+    firstName: string;
+    lastName: string;
+    number: string;
+    dueDate: Date;
+
+    presentationDescription: string;
+    isAnimated: boolean;
+    slides: number;
+    theme: string;
+}
