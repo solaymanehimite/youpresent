e83591a00515d81a8cea73f171f539bb1517ce42
Solaymane Himite <solham.1122@gmail.com>
2025-11-21 14:10:04 +0100

    fix landing page & add mock images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="flex flex-col gap-10 min-h-screen items-start justify-center p-10 sm:px-100 bg-zinc-50 font-sans dark:bg-black">
-            <h1 className="text-5xl font-semibold">Make You're Presentation the right way</h1>
-            <p></p>
+        <div className="flex flex-col gap-10 min-h-screen items-center justify-center p-10 sm:px-100 bg-zinc-50 font-sans dark:bg-black">
+
+            <h1 className="font-semibold text-center">Make You're Presentation <br /> the <span className="text-transparent bg-linear-to-r from-orange-500 to-blue-500 bg-clip-text">right</span> way
+            </h1>
+            <p className="w-md text-center">PresentNow gives you the tools and inspiration to craft stunning, unique presentations with ease.</p>
             <div className="flex gap-2">
                 <Button asChild><Link href="/form">Get Started</Link></Button>
             </div>
